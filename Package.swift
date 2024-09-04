8b80370677fee201b872b8552ac9c927cf1dd793
Simon Bihel <sbihel@users.noreply.github.com>
2024-09-04 13:53:48 +0100

    Add MDoc Reader support (#22)

--- Package.swift
+++ Package.swift
@@ -14,8 +14,7 @@ let package = Package(
             targets: ["SpruceIDMobileSdk"])
     ],
     dependencies: [
-        // .package(url: "https://github.com/spruceid/mobile-sdk-rs.git", .branch("main")),
-        .package(url: "https://github.com/spruceid/mobile-sdk-rs.git", from: "0.0.28"),
+        .package(url: "https://github.com/spruceid/mobile-sdk-rs.git", from: "0.0.29"),
         // .package(path: "../mobile-sdk-rs"),
         .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0")
     ],
