f23afb252d90230a3f1929cd8a526c0619e59f94
Juliano Cézar Chagas Tavares <juliano.cezar@spruceid.com>
2024-10-01 18:45:58 -0300

    Fix dependency version (#37)

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["SpruceIDMobileSdk"])
     ],
     dependencies: [
-        .package(url: "https://github.com/spruceid/mobile-sdk-rs.git", from: "0.0.32"),
+        .package(url: "https://github.com/spruceid/mobile-sdk-rs.git", exact: "0.0.31"),
         // .package(path: "../mobile-sdk-rs"),
         .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0")
     ],
