24de5597e61cff957b3774c0ccd9eeaec302b776
Jacob <jacob.ward@spruceid.com>
2024-10-25 14:50:09 +0100

    [Swift] Credentials and CredentialPack persistence

--- mobile-sdk-swift/Sources/MobileSdk/CredentialPack.swift
+++ mobile-sdk-swift/Sources/MobileSdk/CredentialPack.swift
@@ -2,16 +2,26 @@ import CryptoKit
 import Foundation
 import SpruceIDMobileSdkRs
 
+/// A collection of ParsedCredentials with methods to interact with all instances.
+///
+/// A CredentialPack is a semantic grouping of Credentials for display in the wallet. For example,
+/// the CredentialPack could represent:
+/// - multiple copies of the same credential (for one-time use),
+/// - different encodings of the same credential (JwtVC & JsonVC),
+/// - multiple instances of the same credential type (vehicle title credentials for more than 1 vehicle).
 public class CredentialPack {
+    private var id: UUID
     private var credentials: [ParsedCredential]
 
     /// Initialize an empty CredentialPack.
     public init() {
+        id = UUID()
         credentials = []
     }
 
     /// Initialize a CredentialPack from existing credentials.
-    public init(credentials: [ParsedCredential]) {
+    public init(id: UUID, credentials: [ParsedCredential]) {
+        self.id = id
         self.credentials = credentials
     }
 
@@ -99,4 +109,176 @@ public class CredentialPack {
     public func list() -> [ParsedCredential] {
         return credentials
     }
+
+    /// Persists the CredentialPack in the StorageManager, and persists all credentials in the VdcCollection.
+    ///
+    /// If a credential already exists in the VdcCollection (matching on id), then it will be skipped without updating.
+    public func save(storageManager: StorageManagerInterface) throws {
+        var vdcCollection = VdcCollection(engine: storageManager)
+        for credential in list() {
+            do {
+                if (try vdcCollection.get(id: credential.id())) != nil {
+                    try vdcCollection.add(credential: try credential.intoGenericForm())
+                }
+            } catch {
+                throw CredentialPackError.credentialStorage(id: credential.id(), reason: error)
+            }
+        }
+
+        try CredentialPackContents(id: self.id, credentials: self.credentials.map { credential in
+            credential.id()
+        }).save(storageManager: storageManager)
+    }
+
+    /// Loads all CredentialPacks from the StorageManager.
+    public static func loadAll(storageManager: StorageManagerInterface) throws -> [CredentialPack] {
+        try CredentialPackContents.list(storageManager: storageManager).map { contents in
+            try contents.load(vdcCollection: VdcCollection(engine: storageManager))
+        }
+    }
+}
+
+/// Metadata for a CredentialPack, as loaded from the StorageManager.
+struct CredentialPackContents {
+    private static let storagePrefix = "CredentialPack/"
+    private let idKey = "id"
+    private let credentialsKey = "credentials"
+    let id: UUID
+    let credentials: [Uuid]
+
+    public init(id: UUID, credentials: [Uuid]) {
+        self.id = id
+        self.credentials = credentials
+    }
+
+    public init(fromBytes data: Data) throws {
+        let json: [String: GenericJSON]
+        do {
+            json = try JSONDecoder().decode([String: GenericJSON].self, from: data)
+        } catch {
+            throw CredentialPackError.contentsNotJSON(reason: error)
+        }
+
+        switch json[idKey] {
+        case .string(let id):
+            guard let id = UUID(uuidString: id) else {
+                throw CredentialPackError.idNotUUID(id: id)
+            }
+            self.id = id
+        case nil:
+            throw CredentialPackError.idMissingFromContents
+        default:
+            throw CredentialPackError.idNotString(value: json[idKey]!)
+
+        }
+
+        switch json[credentialsKey] {
+        case .array(let credentialIds):
+            self.credentials = try credentialIds.map { id in
+                switch id {
+                case .string(let id):
+                    id
+                default:
+                    throw CredentialPackError.credentialIdNotString(value: id)
+                }
+            }
+        case nil:
+            throw CredentialPackError.credentialIdsMissingFromContents
+        default:
+            throw CredentialPackError.credentialIdsNotArray(value: json[credentialsKey]!)
+        }
+    }
+
+    /// Loads all of the credentials from the VdcCollection for this CredentialPack.
+    public func load(vdcCollection: VdcCollection) throws -> CredentialPack {
+        let credentials = try credentials.map { credentialId in
+            do {
+                guard let credential = try vdcCollection.get(id: credentialId) else {
+                    throw CredentialPackError.credentialNotFound(id: credentialId)
+                }
+                return try ParsedCredential.parseFromCredential(credential: credential)
+            } catch {
+                throw CredentialPackError.credentialLoading(reason: error)
+            }
+        }
+
+        return CredentialPack(id: self.id, credentials: credentials)
+    }
+
+    /// Lists all CredentialPacks.
+    ///
+    /// These can then be individually loaded. For eager loading of all packs, see `CredentialPack.loadAll`.
+    public static func list(storageManager: StorageManagerInterface) throws -> [CredentialPackContents] {
+        do {
+            return try storageManager.list()
+                .filter { file in
+                    file.hasPrefix(Self.storagePrefix)
+                }
+                .map { file in
+                    guard let contents = try storageManager.get(key: file) else {
+                        throw CredentialPackError.missing(file: file)
+                    }
+                    return try CredentialPackContents(fromBytes: contents)
+                }
+        } catch {
+            throw CredentialPackError.listing(reason: error)
+        }
+    }
+
+    func save(storageManager: StorageManagerInterface) throws {
+        let bytes = try self.toBytes()
+        do {
+            try storageManager.add(key: "\(Self.storagePrefix)\(self.id)", value: bytes)
+        } catch {
+            throw CredentialPackError.storage(reason: error)
+        }
+    }
+
+    private func toBytes() throws -> Data {
+        do {
+            let json = [
+                idKey: GenericJSON.string(self.id.uuidString),
+                credentialsKey: GenericJSON.array(
+                    self.credentials.map { id in
+                        GenericJSON.string(id)
+                    }
+                )
+            ]
+
+            return try JSONEncoder().encode(json)
+        } catch {
+            throw CredentialPackError.contentsEncoding(reason: error)
+        }
+    }
+}
+
+enum CredentialPackError: Error {
+    /// CredentialPackContents file missing from storage.
+    case missing(file: String)
+    /// Failed to list CredentialPackContents from storage.
+    case listing(reason: Error)
+    /// Failed to save CredentialPackContents to storage.
+    case storage(reason: Error)
+    /// Failed to store a new credential when saving a CredentialPack.
+    case credentialStorage(id: Uuid, reason: Error)
+    /// Could not interpret the file payload as JSON when loading a CredentialPackContents from storage.
+    case contentsNotJSON(reason: Error)
+    /// Failed to encode CredentialPackContents as JSON.
+    case contentsEncoding(reason: Error)
+    /// The ID is missing from the CredentialPackContents when loading from storage.
+    case idMissingFromContents
+    /// The CredentialPackContents ID could not be parsed as a JSON String when loading from storage.
+    case idNotString(value: GenericJSON)
+    /// The CredentialPackContents ID could not be parsed as a UUID when loading from storage.
+    case idNotUUID(id: String)
+    /// The credential IDs are missing from the CredentialPackContents when loading from storage.
+    case credentialIdsMissingFromContents
+    /// The CredentialPackContents credential IDs could not be parsed as a JSON Array when loading from storage.
+    case credentialIdsNotArray(value: GenericJSON)
+    /// A CredentialPackContents credential ID could not be parsed as a JSON String when loading from storage.
+    case credentialIdNotString(value: GenericJSON)
+    /// The credential could not be found in storage.
+    case credentialNotFound(id: Uuid)
+    /// The credential could not be loaded from storage.
+    case credentialLoading(reason: Error)
 }
