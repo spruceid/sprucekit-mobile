4cadbbbebfd809b699403ca64eb1fce0efcce0d4
Juliano Cezar Chagas Tavares <julianocezargyn@hotmail.com>
2024-10-29 10:47:39 -0300

    Create delegated verifier

--- mobile-sdk-kt/example/src/main/java/com/spruceid/mobilesdkexample/navigation/Screen.kt
+++ mobile-sdk-kt/example/src/main/java/com/spruceid/mobilesdkexample/navigation/Screen.kt
@@ -4,6 +4,7 @@ const val HOME_SCREEN_PATH = "home"
 const val VERIFY_DL_PATH = "verify_dl"
 const val VERIFY_EA_PATH = "verify_ea"
 const val VERIFY_VC_PATH = "verify_vc"
+const val VERIFY_DELEGATED_OID4VP_PATH = "verify_delegated_oid4vp"
 const val VERIFIER_SETTINGS_HOME_PATH = "verifier_settings_home"
 const val WALLET_SETTINGS_HOME_PATH = "wallet_settings_home"
 const val ADD_TO_WALLET_PATH = "add_to_wallet/{rawCredential}"
@@ -16,6 +17,7 @@ sealed class Screen(val route: String) {
     object VerifyDLScreen : Screen(VERIFY_DL_PATH)
     object VerifyEAScreen : Screen(VERIFY_EA_PATH)
     object VerifyVCScreen : Screen(VERIFY_VC_PATH)
+    object VerifyDelegatedOid4vpScreen : Screen(VERIFY_DELEGATED_OID4VP_PATH)
     object VerifierSettingsHomeScreen : Screen(VERIFIER_SETTINGS_HOME_PATH)
     object WalletSettingsHomeScreen : Screen(WALLET_SETTINGS_HOME_PATH)
     object AddToWalletScreen : Screen(ADD_TO_WALLET_PATH)
