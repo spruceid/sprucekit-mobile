bb1d33405535d49d62ee491b0104161e27692506
Ryan Tate <ryan.tate@spruceid.com>
2024-11-01 10:00:26 -0700

    fix rust lint test
    Signed-off-by: Ryan Tate <ryan.tate@spruceid.com>

--- mobile-sdk-rs/src/oid4vp/verifier.rs
+++ mobile-sdk-rs/src/oid4vp/verifier.rs
@@ -137,7 +137,7 @@ mod tests {
             .await
             .expect("Failed to request delegated verification");
 
-        let DelegatedVerifierStatusResponse { status, oid4vp } =
+        let DelegatedVerifierStatusResponse { status, .. } =
             verifier.poll_verification_status(&uri).await?;
 
         assert_eq!(status, DelegatedVerifierStatus::Initiated);
@@ -182,7 +182,7 @@ mod tests {
         assert_eq!(status, DelegatedVerifierStatus::Success);
         assert!(oid4vp.is_some());
 
-        println!("Presentation: {oid4vp:?}");
+        println!("Presentation: {:?}", oid4vp);
 
         Ok(())
     }
