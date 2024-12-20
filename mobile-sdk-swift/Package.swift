2698e66dbac6b75902a1f0b9c66ada6f5c447ff5
Juliano Cezar Chagas Tavares <julianocezargyn@hotmail.com>
2024-12-20 13:37:35 -0300

    Fix swiftlint

--- mobile-sdk-swift/Package.swift
+++ mobile-sdk-swift/Package.swift
@@ -16,18 +16,18 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/spruceid/mobile-sdk-rs.git", from: "0.4.3"),
         // .package(path: "../mobile-sdk-rs"),
-        .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0")
     ],
     targets: [
         .target(
             name: "SpruceIDMobileSdk",
             dependencies: [
                 .product(name: "SpruceIDMobileSdkRs", package: "mobile-sdk-rs"),
-                .product(name: "Algorithms", package: "swift-algorithms"),
+                .product(name: "Algorithms", package: "swift-algorithms")
             ]
         ),
         .testTarget(
             name: "SpruceIDMobileSdkTests",
-            dependencies: ["SpruceIDMobileSdk"]),
+            dependencies: ["SpruceIDMobileSdk"])
     ]
 )
