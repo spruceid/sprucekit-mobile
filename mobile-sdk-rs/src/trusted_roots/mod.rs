f60f5e2e2c10ee2da24b4efcea6ed367f5df1c07
Gregorio <greg.magalhaes@gmail.com>
2025-03-04 16:28:50 -0300

    Remove file header

--- mobile-sdk-rs/src/trusted_roots/mod.rs
+++ mobile-sdk-rs/src/trusted_roots/mod.rs
@@ -1,14 +1,3 @@
-// certificates.rs
-//
-// SPRUCE SYSTEMS, INC. PROPRIETARY AND CONFIDENTIAL.
-//
-// Spruce Systems, Inc. Copyright 2025. All Rights Reserved. Spruce Systems, Inc.
-// retains sole and exclusive, right, title and interest in and to all code, Work
-// Product and other deliverables, and all copies, modifications, and derivative
-// works thereof, including all proprietary or intellectual property rights
-// contained therein. The file may not be used or distributed without express
-// permission of Spruce Systems, Inc.
-
 use x509_cert::{der::Decode as _, Certificate};
 
 const SPRUCE_COUNTY_ROOT_CERTIFICATE_DER: &[u8] = include_bytes!("./spruce_county.der");
