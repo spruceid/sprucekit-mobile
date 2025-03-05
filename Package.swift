53a21c85a608ca92092113de11399b7db10fec1a
Simon Bihel <simon.bihel@spruceid.com>
2025-03-05 15:12:34 +0000

    Fix permission issue in cd action
    Also:
    - revert the change to package.swift
    - improve the action to handle failure better

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0")
   ],
   targets: [
-    .binaryTarget(name: "RustFramework", url: "https://github.com/spruceid/sprucekit-mobile/releases/download/0.10.0/RustFramework.xcframework.zip", checksum: "7136dc823817489b585afc73f8e5b3e0721095ddaa919b59c12440f5ef02f426"),
+    .binaryTarget(name: "RustFramework", path: "rust/MobileSdkRs/RustFramework.xcframework"),
     .target(
       name: "SpruceIDMobileSdkRs",
       dependencies: [
