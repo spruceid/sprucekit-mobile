d6bb7b4f6cd558583c35a14b6d7459b67b166551
Marcelo <marceloer2011@gmail.com>
2025-06-27 15:04:58 -0300

    Add new empty wallet page for Android v1

--- android/Showcase/src/main/java/com/spruceid/mobilesdkexample/wallet/WalletHomeView.kt
+++ android/Showcase/src/main/java/com/spruceid/mobilesdkexample/wallet/WalletHomeView.kt
@@ -1,21 +1,28 @@
 package com.spruceid.mobilesdkexample.wallet
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -28,9 +35,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,8 +59,12 @@ import com.spruceid.mobilesdkexample.LoadingView
 import com.spruceid.mobilesdkexample.R
 import com.spruceid.mobilesdkexample.db.WalletActivityLogs
 import com.spruceid.mobilesdkexample.navigation.Screen
+import com.spruceid.mobilesdkexample.ui.theme.ColorBase100
 import com.spruceid.mobilesdkexample.ui.theme.ColorBase150
+import com.spruceid.mobilesdkexample.ui.theme.ColorBlue100
+import com.spruceid.mobilesdkexample.ui.theme.ColorBlue600
 import com.spruceid.mobilesdkexample.ui.theme.ColorStone400
+import com.spruceid.mobilesdkexample.ui.theme.ColorStone600
 import com.spruceid.mobilesdkexample.ui.theme.ColorStone950
 import com.spruceid.mobilesdkexample.ui.theme.Inter
 import com.spruceid.mobilesdkexample.utils.credentialDisplaySelector
@@ -55,6 +76,7 @@ import com.spruceid.mobilesdkexample.viewmodels.HacApplicationsViewModel
 import com.spruceid.mobilesdkexample.viewmodels.HelpersViewModel
 import com.spruceid.mobilesdkexample.viewmodels.StatusListViewModel
 import com.spruceid.mobilesdkexample.viewmodels.WalletActivityLogsViewModel
+import com.spruceid.mobilesdkexample.walletsettings.generateMockMdl
 import kotlinx.coroutines.launch
 
 @Composable
@@ -265,14 +287,17 @@ fun WalletHomeBody(
         } else {
             Box(Modifier.fillMaxSize()) {
                 Column(
-                    Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.empty_wallet),
-                        contentDescription = stringResource(id = R.string.empty_wallet),
-                    )
+                    Spacer(modifier = Modifier.height(30.dp))
+                    NoCredentialCard(onGenerateMockMdl = {
+                        isRefreshing = true
+                        scope.launch {
+                            generateMockMdl(credentialPacksViewModel)
+                        }
+                        isRefreshing = false
+                    })
                 }
             }
         }
@@ -280,5 +305,95 @@ fun WalletHomeBody(
         LoadingView(
             loadingText = ""
         )
+    }
+}
+@Composable
+fun NoCredentialCard(
+    onGenerateMockMdl: () -> Unit
+) {
+    AnimatedVisibility (
+        visible = true,
+        enter = EnterTransition.None,
+        exit = fadeOut()
+    ) {
+        Box(
+            modifier = Modifier
+                // Shadow is not quite accurate
+                .shadow(
+                    elevation = 8.dp,
+                    shape = RoundedCornerShape(12.dp),
+                    ambientColor = Color.Black.copy(alpha = 0.6f),
+                    spotColor = Color.Black.copy(alpha = 0.6f),
+                )
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(ColorBase100, ColorBlue100),
+                    ),
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .border(
+                    width = 2.dp,
+                    color = Color.White,
+                    shape = RoundedCornerShape(12.dp)
+                )
+                .padding(start = 20.dp, top = 24.dp, end = 20.dp, bottom = 16.dp)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(36.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // Title + Subtitle
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Text(
+                        text = "Welcome!",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.W600,
+                        color = ColorBlue600
+                    )
+                    Text(
+                        text = "You currently have no credentials in your wallet",
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.W500,
+                        color = ColorStone600
+                    )
+                }
+
+                // Image (mDL)
+                Image(
+                    painter = painterResource(id = R.drawable.mdl_image),
+                    contentDescription = "mDL Image"
+                )
+
+                // Button
+                Button(
+                    onClick = onGenerateMockMdl,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = ColorBlue600,
+                        contentColor = Color.White
+                    ),
+                    shape = RoundedCornerShape(100.dp),
+                    modifier = Modifier
+                        .height(55.dp)
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp, vertical = 8.dp)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.generate_mdl),
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp),
+                        tint = Color.White
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
+                    Text(
+                        text = "Generate a Spruce mDL",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.W400
+                    )
+                }
+            }
+        }
     }
 }
