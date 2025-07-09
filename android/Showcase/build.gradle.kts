4ffae9defc11528f0244d0c6d5d5304e0711b55d
Juliano Cezar Chagas Tavares <julianocezargyn@hotmail.com>
2025-07-09 09:59:11 -0300

    Fix redirect

--- android/Showcase/build.gradle.kts
+++ android/Showcase/build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.navigation:navigation-compose:2.8.4")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("app.rive:rive-android:8.7.0")
     implementation("androidx.startup:startup-runtime:1.1.1")
