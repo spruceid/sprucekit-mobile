5ef761dc18016e5cb5720283b8e389d8e0b13160
Juliano Cezar Chagas Tavares <julianocezargyn@hotmail.com>
2025-07-15 14:58:47 -0300

    Bump dependencies

--- android/MobileSdk/build.gradle.kts
+++ android/MobileSdk/build.gradle.kts
@@ -116,6 +116,7 @@ android {
     }
 
     composeOptions { kotlinCompilerExtensionVersion = "1.5.11" }
+    ndkVersion = "29.0.13599879 rc2"
 
     publishing {
         singleVariant("release") {
@@ -129,22 +130,22 @@ dependencies {
     api(project(":MobileSdkRs"))
     //noinspection GradleCompatible
     implementation("com.android.support:appcompat-v7:28.0.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.activity:activity-compose:1.10.1")
     /* Begin UI dependencies */
-    implementation("androidx.compose.material3:material3:1.2.1")
-    implementation("androidx.camera:camera-camera2:1.3.2")
-    implementation("androidx.camera:camera-lifecycle:1.3.2")
-    implementation("androidx.camera:camera-view:1.3.2")
-    implementation("com.google.zxing:core:3.5.1")
-    implementation("com.google.accompanist:accompanist-permissions:0.34.0")
-    implementation("androidx.camera:camera-mlkit-vision:1.3.0-alpha06")
-    implementation("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
+    implementation("androidx.compose.material3:material3:1.3.2")
+    implementation("androidx.camera:camera-camera2:1.4.2")
+    implementation("androidx.camera:camera-lifecycle:1.4.2")
+    implementation("androidx.camera:camera-view:1.4.2")
+    implementation("com.google.zxing:core:3.5.3")
+    implementation("com.google.accompanist:accompanist-permissions:0.37.3")
+    implementation("androidx.camera:camera-mlkit-vision:1.4.2")
+    implementation("com.google.android.gms:play-services-mlkit-text-recognition:19.0.1")
     /* End UI dependencies */
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
     implementation("com.google.android.play:integrity:1.4.0")
     implementation("org.bitbucket.b_c:jose4j:0.9.6")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.json:json:20230618")
+    testImplementation("org.json:json:20250517")
     androidTestImplementation("com.android.support.test:runner:1.0.2")
     androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
 
@@ -155,7 +156,7 @@ dependencies {
     implementation("androidx.credentials.registry:registry-digitalcredentials-mdoc:$androidxCredentialsVersion")
     implementation("androidx.credentials.registry:registry-provider:$androidxCredentialsVersion")
     implementation("androidx.credentials.registry:registry-provider-play-services:$androidxCredentialsVersion")
-    implementation("com.google.android.gms:play-services-identity-credentials:16.0.0-alpha06")
+    implementation("com.google.android.gms:play-services-identity-credentials:16.0.0-alpha08")
 }
 
 configurations.all {
