7bed369b66e5ed15ac970db8d668bb9970cf18cc
Juliano Cezar Chagas Tavares <juliano.cezar@spruceid.com>
2025-07-16 10:23:13 -0300

    Make SpruceKit Showcase, MobileSdk, and MobileSdkRs compatible with Android 15 (#175)
    This bumps dependencies and adds settings to make the project compatible with Android 15 devices (required by Nov 2025).

--- android/Showcase/build.gradle.kts
+++ android/Showcase/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.spruceid.mobilesdkexample"
         minSdk = 26
         targetSdk = 35
-        versionCode = 55
-        versionName = "1.13.2"
+        versionCode = 56
+        versionName = "1.14.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -54,6 +54,7 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    ndkVersion = "29.0.13599879 rc2"
 }
 
 ksp {
@@ -61,8 +62,8 @@ ksp {
 }
 
 dependencies {
-    val roomVersion = "2.6.1"
-    val hiltVersion = "2.51.1"
+    val roomVersion = "2.7.2"
+    val hiltVersion = "2.56.2"
 
     implementation("androidx.room:room-runtime:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
@@ -70,31 +71,31 @@ dependencies {
     annotationProcessor("androidx.room:room-compiler:$roomVersion")
     implementation("com.google.dagger:hilt-android:$hiltVersion")
     kapt("com.google.dagger:hilt-compiler:$hiltVersion")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
-    implementation("androidx.activity:activity-compose:1.9.3")
-    implementation(platform("androidx.compose:compose-bom:2025.05.01"))
-    implementation("com.google.accompanist:accompanist-permissions:0.35.1-alpha")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(platform("androidx.compose:compose-bom:2025.06.01"))
+    implementation("com.google.accompanist:accompanist-permissions:0.37.3")
     implementation("com.google.accompanist:accompanist-swiperefresh:0.36.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
-    implementation("app.rive:rive-android:8.7.0")
-    implementation("androidx.startup:startup-runtime:1.1.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+    implementation("app.rive:rive-android:10.2.1")
+    implementation("androidx.startup:startup-runtime:1.2.0")
     implementation(project(mapOf("path" to ":MobileSdk")))
-    implementation("com.google.zxing:core:3.5.1")
-    implementation("io.ktor:ktor-client-core:2.3.12")
-    implementation("io.ktor:ktor-client-cio:2.3.12")
-    implementation("io.coil-kt.coil3:coil-compose:3.0.0-rc01")
-    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.0-rc01")
+    implementation("com.google.zxing:core:3.5.3")
+    implementation("io.ktor:ktor-client-core:3.2.2")
+    implementation("io.ktor:ktor-client-cio:3.2.2")
+    implementation("io.coil-kt.coil3:coil-compose:3.2.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.2.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2025.05.01"))
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.06.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
