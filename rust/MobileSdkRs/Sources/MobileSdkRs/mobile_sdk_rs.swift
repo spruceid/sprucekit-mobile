dc4e65592c0c592ac4ecbd49c6e8131063a54cc3
Ryan Tate <ryan.tate@spruceid.com>
2025-09-09 13:42:12 -0700

    enable csv activity log export
    Signed-off-by: Ryan Tate <ryan.tate@spruceid.com>

--- rust/MobileSdkRs/Sources/MobileSdkRs/mobile_sdk_rs.swift
+++ rust/MobileSdkRs/Sources/MobileSdkRs/mobile_sdk_rs.swift
@@ -605,6 +605,11 @@ public protocol ActivityLogProtocol: AnyObject, Sendable {
      */
     func exportEntries(filter: ActivityLogFilterOptions?) async throws  -> String
     
+    /**
+     * Returns the optionally filtered activity log entries list as CSV encoded string for export use.
+     */
+    func exportEntriesCsv(filter: ActivityLogFilterOptions?) async throws  -> String
+    
     func get(entryId: Uuid) async throws  -> ActivityLogEntry?
     
     func remove(entryId: Uuid) async throws 
@@ -762,6 +767,26 @@ open func exportEntries(filter: ActivityLogFilterOptions?)async throws  -> Strin
         )
 }
     
+    /**
+     * Returns the optionally filtered activity log entries list as CSV encoded string for export use.
+     */
+open func exportEntriesCsv(filter: ActivityLogFilterOptions?)async throws  -> String  {
+    return
+        try  await uniffiRustCallAsync(
+            rustFutureFunc: {
+                uniffi_mobile_sdk_rs_fn_method_activitylog_export_entries_csv(
+                    self.uniffiClonePointer(),
+                    FfiConverterOptionTypeActivityLogFilterOptions.lower(filter)
+                )
+            },
+            pollFunc: ffi_mobile_sdk_rs_rust_future_poll_rust_buffer,
+            completeFunc: ffi_mobile_sdk_rs_rust_future_complete_rust_buffer,
+            freeFunc: ffi_mobile_sdk_rs_rust_future_free_rust_buffer,
+            liftFunc: FfiConverterString.lift,
+            errorHandler: FfiConverterTypeActivityLogError_lift
+        )
+}
+    
 open func get(entryId: Uuid)async throws  -> ActivityLogEntry?  {
     return
         try  await uniffiRustCallAsync(
@@ -875,7 +900,7 @@ public protocol ActivityLogEntryProtocol: AnyObject, Sendable {
     
     func getCredentialId()  -> Uuid
     
-    func getDate()  -> UInt64
+    func getDate()  -> String
     
     func getDescription()  -> String
     
@@ -886,6 +911,8 @@ public protocol ActivityLogEntryProtocol: AnyObject, Sendable {
     func getId()  -> Uuid
     
     func getInteractionWith()  -> String
+    
+    func getTimestamp()  -> UInt64
     
     func getType()  -> ActivityLogEntryType
     
@@ -990,8 +1017,8 @@ open func getCredentialId() -> Uuid  {
 })
 }
     
-open func getDate() -> UInt64  {
-    return try!  FfiConverterUInt64.lift(try! rustCall() {
+open func getDate() -> String  {
+    return try!  FfiConverterString.lift(try! rustCall() {
     uniffi_mobile_sdk_rs_fn_method_activitylogentry_get_date(self.uniffiClonePointer(),$0
     )
 })
@@ -1028,6 +1055,13 @@ open func getId() -> Uuid  {
 open func getInteractionWith() -> String  {
     return try!  FfiConverterString.lift(try! rustCall() {
     uniffi_mobile_sdk_rs_fn_method_activitylogentry_get_interaction_with(self.uniffiClonePointer(),$0
+    )
+})
+}
+    
+open func getTimestamp() -> UInt64  {
+    return try!  FfiConverterUInt64.lift(try! rustCall() {
+    uniffi_mobile_sdk_rs_fn_method_activitylogentry_get_timestamp(self.uniffiClonePointer(),$0
     )
 })
 }
@@ -21383,6 +21417,9 @@ private let initializationResult: InitializationResult = {
     if (uniffi_mobile_sdk_rs_checksum_method_activitylog_export_entries() != 39487) {
         return InitializationResult.apiChecksumMismatch
     }
+    if (uniffi_mobile_sdk_rs_checksum_method_activitylog_export_entries_csv() != 7492) {
+        return InitializationResult.apiChecksumMismatch
+    }
     if (uniffi_mobile_sdk_rs_checksum_method_activitylog_get() != 49762) {
         return InitializationResult.apiChecksumMismatch
     }
@@ -21395,7 +21432,7 @@ private let initializationResult: InitializationResult = {
     if (uniffi_mobile_sdk_rs_checksum_method_activitylogentry_get_credential_id() != 4490) {
         return InitializationResult.apiChecksumMismatch
     }
-    if (uniffi_mobile_sdk_rs_checksum_method_activitylogentry_get_date() != 5229) {
+    if (uniffi_mobile_sdk_rs_checksum_method_activitylogentry_get_date() != 6365) {
         return InitializationResult.apiChecksumMismatch
     }
     if (uniffi_mobile_sdk_rs_checksum_method_activitylogentry_get_description() != 26368) {
@@ -21411,6 +21448,9 @@ private let initializationResult: InitializationResult = {
         return InitializationResult.apiChecksumMismatch
     }
     if (uniffi_mobile_sdk_rs_checksum_method_activitylogentry_get_interaction_with() != 19643) {
+        return InitializationResult.apiChecksumMismatch
+    }
+    if (uniffi_mobile_sdk_rs_checksum_method_activitylogentry_get_timestamp() != 63696) {
         return InitializationResult.apiChecksumMismatch
     }
     if (uniffi_mobile_sdk_rs_checksum_method_activitylogentry_get_type() != 42347) {
--- rust/src/credential/activity_log.rs
+++ rust/src/credential/activity_log.rs
@@ -1,4 +1,4 @@
-use std::{sync::Arc, time::SystemTime};
+use std::sync::Arc;
 
 use crate::{storage_manager::StorageManagerInterface, Key, Value};
 
@@ -65,13 +65,13 @@ impl ActivityLogFilterOptions {
     fn should_filter_entry(&self, entry: &ActivityLogEntry, index: usize) -> bool {
         // Check date range filters
         if let Some(from_date) = self.from_date {
-            if from_date > entry.date {
+            if from_date > entry.timestamp {
                 return false;
             }
         }
 
         if let Some(to_date) = self.to_date {
-            if to_date < entry.date {
+            if to_date < entry.timestamp {
                 return false;
             }
         }
@@ -110,8 +110,10 @@ pub struct ActivityLogEntry {
     credential_id: Uuid,
     /// Type of activity log entry
     r#type: ActivityLogEntryType,
-    /// date encoded as a unix timestamp
-    date: u64,
+    /// Date encoded as a UNIX timestamp
+    timestamp: u64,
+    /// Date encoded as an RFC 3339 and ISO 8601 date and time string such as `1996-12-19T16:39:57-08:00`.
+    date: String,
     /// Description shown the user of the interaction
     description: String,
     /// Interaction with denotes the name of the
@@ -157,10 +159,9 @@ impl ActivityLogEntry {
         fields: Option<Vec<String>>,
         url: Option<String>,
     ) -> Result<Self, ActivityLogError> {
-        let date = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .map_err(|e| ActivityLogError::CreateActivityLogEntry(e.to_string()))?
-            .as_secs();
+        let now = chrono::Utc::now();
+        let timestamp = now.timestamp() as u64;
+        let date = now.to_rfc3339();
 
         let fields = fields.unwrap_or_default();
 
@@ -168,6 +169,7 @@ impl ActivityLogEntry {
             id: Uuid::new_v4(),
             credential_id,
             r#type,
+            timestamp,
             date,
             description,
             interaction_with,
@@ -203,8 +205,12 @@ impl ActivityLogEntry {
         self.r#type.clone()
     }
 
-    fn get_date(&self) -> u64 {
-        self.date
+    fn get_date(&self) -> String {
+        self.date.clone()
+    }
+
+    fn get_timestamp(&self) -> u64 {
+        self.timestamp
     }
 
     fn get_description(&self) -> String {
@@ -395,6 +401,28 @@ impl ActivityLog {
             .collect::<Vec<ActivityLogEntry>>();
         serde_json::to_string(&entries)
             .map_err(|e| ActivityLogError::ActivityLogEntrySerialization(e.to_string()))
+    }
+
+    /// Returns the optionally filtered activity log entries list as CSV encoded string for export use.
+    pub async fn export_entries_csv(
+        &self,
+        filter: Option<ActivityLogFilterOptions>,
+    ) -> Result<String, ActivityLogError> {
+        let mut wtr = csv::Writer::from_writer(Vec::new());
+
+        for entry in self.filter_entries(filter).await?.into_iter() {
+            wtr.serialize(&entry)
+                .map_err(|e| ActivityLogError::ActivityLogEntrySerialization(e.to_string()))?;
+        }
+
+        let bytes = wtr
+            .into_inner()
+            .map_err(|e| ActivityLogError::ActivityLogEntrySerialization(e.to_string()))?;
+
+        let data = String::from_utf8(bytes.to_owned())
+            .map_err(|e| ActivityLogError::ActivityLogEntrySerialization(e.to_string()))?;
+
+        Ok(data)
     }
 }
 
