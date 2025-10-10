3e19a9fe3e27f9c654af680e2b74e60ab31ca936
Marcelo Eduardo R. <marceloer2011@gmail.com>
2025-10-10 16:21:10 -0300

    [ACC-805] - Implement new header style (#206)

--- android/Showcase/src/main/java/com/spruceid/mobilesdkexample/wallet/WalletHomeView.kt
+++ android/Showcase/src/main/java/com/spruceid/mobilesdkexample/wallet/WalletHomeView.kt
@@ -1,22 +1,15 @@
 package com.spruceid.mobilesdkexample.wallet
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -27,13 +20,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
@@ -42,10 +31,11 @@ import com.spruceid.mobilesdkexample.LoadingView
 import com.spruceid.mobilesdkexample.R
 import com.spruceid.mobilesdkexample.db.WalletActivityLogs
 import com.spruceid.mobilesdkexample.navigation.Screen
-import com.spruceid.mobilesdkexample.ui.theme.ColorBase150
-import com.spruceid.mobilesdkexample.ui.theme.ColorStone400
-import com.spruceid.mobilesdkexample.ui.theme.ColorStone950
-import com.spruceid.mobilesdkexample.ui.theme.Inter
+import com.spruceid.mobilesdkexample.ui.components.HeaderButton
+import com.spruceid.mobilesdkexample.ui.components.HomeHeader
+import com.spruceid.mobilesdkexample.ui.theme.ColorAmber600
+import com.spruceid.mobilesdkexample.ui.theme.ColorBase1
+import com.spruceid.mobilesdkexample.ui.theme.ColorBlue600
 import com.spruceid.mobilesdkexample.utils.activityHiltViewModel
 import com.spruceid.mobilesdkexample.utils.credentialDisplaySelector
 import com.spruceid.mobilesdkexample.utils.getCredentialIdTitleAndIssuer
@@ -63,71 +53,48 @@ import kotlinx.coroutines.launch
 fun WalletHomeView(
     navController: NavController
 ) {
-    Column(
-        Modifier
-            .padding(all = 20.dp)
-            .padding(top = 30.dp)
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
-        WalletHomeHeader(navController = navController)
-        WalletHomeBody(
-            navController = navController
-        )
+        Column(
+            Modifier.fillMaxSize()
+        ) {
+            WalletHomeHeader(navController = navController)
+            Column(
+                Modifier.padding(horizontal = 20.dp)
+            ) {
+                WalletHomeBody(
+                    navController = navController
+                )
+            }
+
+        }
     }
 }
 
 @Composable
-fun WalletHomeHeader(navController: NavController) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Text(
-            text = "Wallet",
-            fontFamily = Inter,
-            fontWeight = FontWeight.SemiBold,
-            fontSize = 20.sp,
-            color = ColorStone950
+fun WalletHomeHeader(
+    navController: NavController?
+) {
+    val gradientColors = listOf(ColorBlue600, ColorBase1)
+    val buttons = listOf(
+        HeaderButton(
+            icon = painterResource(id = R.drawable.qrcode_scanner),
+            contentDescription = stringResource(id = R.string.qrcode_scanner),
+            onClick = { navController?.navigate(Screen.ScanQRScreen.route) }
+        ),
+        HeaderButton(
+            icon = painterResource(id = R.drawable.user),
+            contentDescription = stringResource(id = R.string.user),
+            onClick = { navController?.navigate(Screen.WalletSettingsHomeScreen.route) }
         )
-        Spacer(Modifier.weight(1f))
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier =
-                Modifier
-                    .width(36.dp)
-                    .height(36.dp)
-                    .padding(start = 4.dp)
-                    .clip(shape = RoundedCornerShape(8.dp))
-                    .background(ColorBase150)
-                    .clickable { navController.navigate(Screen.ScanQRScreen.route) }
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.qrcode_scanner),
-                contentDescription = stringResource(id = R.string.qrcode_scanner),
-                colorFilter = ColorFilter.tint(ColorStone400),
-                modifier = Modifier
-                    .width(20.dp)
-                    .height(20.dp)
-            )
-        }
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier =
-                Modifier
-                    .width(36.dp)
-                    .height(36.dp)
-                    .padding(start = 4.dp)
-                    .clip(shape = RoundedCornerShape(8.dp))
-                    .background(ColorBase150)
-                    .clickable {
-                        navController.navigate(Screen.WalletSettingsHomeScreen.route)
-                    }
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.user),
-                contentDescription = stringResource(id = R.string.user),
-                modifier = Modifier
-                    .width(20.dp)
-                    .height(20.dp)
-            )
-        }
-    }
+    )
+
+    HomeHeader(
+        title = "Wallet",
+        gradientColors = gradientColors,
+        buttons = buttons
+    )
 }
 
 @Composable
