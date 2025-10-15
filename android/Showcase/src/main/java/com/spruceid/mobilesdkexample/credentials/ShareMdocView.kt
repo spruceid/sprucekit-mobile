e25d538edf0690ab019e6b3771e61e26cade037d
Marcelo Eduardo R. <marceloer2011@gmail.com>
2025-10-15 10:28:35 -0300

    Android UI: Implementing some minor feedbacks from Elissa (#212)

--- android/Showcase/src/main/java/com/spruceid/mobilesdkexample/credentials/ShareMdocView.kt
+++ android/Showcase/src/main/java/com/spruceid/mobilesdkexample/credentials/ShareMdocView.kt
@@ -55,7 +55,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
