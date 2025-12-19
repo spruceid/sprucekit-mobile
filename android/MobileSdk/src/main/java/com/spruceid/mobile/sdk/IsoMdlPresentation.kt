2ea7814a710bd646921f782048beb8df594fcd85
Ryan Tate <ryan.tate@spruceid.com>
2025-12-19 08:38:48 -0800

    add a reader name isomdl session callback event (#254)
    Signed-off-by: Ryan Tate <ryan.tate@spruceid.com>

--- android/MobileSdk/src/main/java/com/spruceid/mobile/sdk/IsoMdlPresentation.kt
+++ android/MobileSdk/src/main/java/com/spruceid/mobile/sdk/IsoMdlPresentation.kt
@@ -294,6 +294,8 @@ class IsoMdlPresentation(
         try {
             this.itemsRequests = session!!.handleRequest(data)
             this.callback.update(mapOf(Pair("selectNamespaces", this.itemsRequests)))
+            // Return a callback providing the verifier / reader's common name requesting the data
+            this.callback.update(mapOf(Pair("readerName", session!!.readerName())))
         } catch (e: RequestException) {
             Log.e("IsoMdlPresentation", "Error handling request: ${e.message}", e)
             this.callback.error(e)
