55712f82a34976dd440d303cb2552170d9bf0865
Juliano Cezar Chagas Tavares <julianocezargyn@hotmail.com>
2026-05-06 16:45:57 -0300

    Flutter android referencing local dependency

--- flutter/example/android/settings.gradle.kts
+++ flutter/example/android/settings.gradle.kts
@@ -17,6 +17,19 @@ pluginManagement {
     }
 }
 
+// Local-only: build the SpruceKit Android SDK from source instead of consuming
+// the published Maven Central artifact. Lets you iterate on `android/MobileSdk`
+// (and `android/MobileSdkRs`) and have `flutter run` pick up the changes
+// without `publishToMavenLocal`. Remove or comment out for normal builds.
+includeBuild("../../../android") {
+    dependencySubstitution {
+        substitute(module("com.spruceid.mobile.sdk:mobilesdk"))
+            .using(project(":MobileSdk"))
+        substitute(module("com.spruceid.mobile.sdk.rs:mobilesdkrs"))
+            .using(project(":MobileSdkRs"))
+    }
+}
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.11.1" apply false
