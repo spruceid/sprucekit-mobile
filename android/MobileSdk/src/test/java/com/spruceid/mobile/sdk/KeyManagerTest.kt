48db55b4a11505803df75e54a6eaefb19cd1f256
Patrick Maltagliati <patrick.maltagliati@spruceid.com>
2026-07-07 12:25:52 -0500

    Explicitly generate a random 96-bit IV from OS entropy (CWE-327 defense-in-depth).

--- android/MobileSdk/src/test/java/com/spruceid/mobile/sdk/KeyManagerTest.kt
+++ android/MobileSdk/src/test/java/com/spruceid/mobile/sdk/KeyManagerTest.kt
@@ -35,4 +35,74 @@ class KeyManagerTest {
 
         assertArrayEquals(inputEqualTo, resultEqualTo)
     }
+
+    @Test
+    fun directKeyEncryptionRoundtrip() {
+        val keyManager = KeyManager()
+        val key = ByteArray(32) { it.toByte() }
+        val data = "some payload to protect".toByteArray()
+
+        val (iv, encrypted) = keyManager.encryptWithDirectKey(key, data)
+        val decrypted = keyManager.decryptWithDirectKey(key, iv, encrypted)
+
+        assertArrayEquals(data, decrypted)
+    }
+
+    @Test
+    fun directKeyEncryptionRoundtripEmptyPayload() {
+        val keyManager = KeyManager()
+        val key = ByteArray(32) { it.toByte() }
+
+        val (iv, encrypted) = keyManager.encryptWithDirectKey(key, ByteArray(0))
+        val decrypted = keyManager.decryptWithDirectKey(key, iv, encrypted)
+
+        assertArrayEquals(ByteArray(0), decrypted)
+    }
+
+    @Test
+    fun directKeyEncryptionUses96BitIv() {
+        val keyManager = KeyManager()
+        val key = ByteArray(32) { it.toByte() }
+
+        val (iv, _) = keyManager.encryptWithDirectKey(key, "data".toByteArray())
+
+        assertEquals(12, iv.size)
+    }
+
+    @Test
+    fun directKeyEncryptionGeneratesUniqueIvs() {
+        val keyManager = KeyManager()
+        val key = ByteArray(32) { it.toByte() }
+        val data = "same data".toByteArray()
+
+        val ivs = (1..10).map { keyManager.encryptWithDirectKey(key, data).first.toList() }
+
+        assertEquals(10, ivs.toSet().size)
+    }
+
+    @Test
+    fun directKeyDecryptionFailsOnTamperedCiphertext() {
+        val keyManager = KeyManager()
+        val key = ByteArray(32) { it.toByte() }
+
+        val (iv, encrypted) = keyManager.encryptWithDirectKey(key, "data".toByteArray())
+        encrypted[0] = (encrypted[0].toInt() xor 0x01).toByte()
+
+        assertThrows(Exception::class.java) {
+            keyManager.decryptWithDirectKey(key, iv, encrypted)
+        }
+    }
+
+    @Test
+    fun directKeyDecryptionFailsWithWrongKey() {
+        val keyManager = KeyManager()
+        val key = ByteArray(32) { it.toByte() }
+        val wrongKey = ByteArray(32) { (it + 1).toByte() }
+
+        val (iv, encrypted) = keyManager.encryptWithDirectKey(key, "data".toByteArray())
+
+        assertThrows(Exception::class.java) {
+            keyManager.decryptWithDirectKey(wrongKey, iv, encrypted)
+        }
+    }
 }
